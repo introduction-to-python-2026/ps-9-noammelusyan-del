20cb5575224702ae9f935831f6fdccb9dba7985f
noammelusyan-del <noammelusyan@gmail.com>
2025-12-25 18:27:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# Download the data from your GitHub repository
-!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
-!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
-import lab_setup_do_not_edit
-
 import pandas as pd
 
 df = pd.read_csv('parkinsons.csv')
